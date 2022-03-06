06bcff2725f775fef8c920d4c5a7885ca248f27b
Artem <artem.horuzhenko@softeq.com>
2022-03-06 16:10:09 +0300

    fix paths

--- .circleci/redisstack/dmg.repack.sh
+++ .circleci/redisstack/dmg.repack.sh
@@ -12,8 +12,8 @@ mkdir -p "$WORKING_DIRECTORY/release/redisstack"
 mkdir -p "$TMP_FOLDER"
 
 hdiutil attach "./release/RedisInsight-preview-mac-$ARCH.dmg"
-cp -r /Volumes/RedisInsight-*/RedisInsight-preview.app/* "$TMP_FOLDER"
-cd "$TMP_FOLDER" || exit 1
+cp -r /Volumes/RedisInsight-*/RedisInsight-preview.app "/tmp"
+cd "/tmp" || exit 1
 tar -czvf "$TAR_NAME" "$APP_FOLDER_NAME"
 cp "$TAR_NAME" "$WORKING_DIRECTORY/release/redisstack/"
 cd "$WORKING_DIRECTORY" || exit 1
